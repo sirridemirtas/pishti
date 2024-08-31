09a1b8a556498de8f3f4349c837e687f2620daba
Sirri Demirtas <sirridemirtas@gmail.com>
2024-08-31 14:42:13 +0300

    Created Manager to handle the game

---
+++ include/Manager.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <map>
+#include <vector>
+
+#include "Pishti.h"
+#include "Player.h"
+
+// This class is responsible for managing the game
+class Manager {
+    Pishti *game;
+
+    std::map<PishtiEvent, std::vector<Player *> > eventSubscribers;
+
+    void subscribeToEvent(PishtiEvent event, Player *player);
+
+    void notifyPlayers(PishtiEvent event, void *payload);
+
+    void setupTableCards();
+
+    void dealCards();
+
+    void updateScores();
+
+public:
+    Manager();
+
+    void addPlayer(Player *player);
+
+    void removePlayer(Player *player);
+
+    size_t getNumPlayers() const;
+
+    void setGame(Pishti *game);
+
+    void initGame();
+
+    void playRound();
+
+    void endGame();
+
+    void resetGame();
+};
